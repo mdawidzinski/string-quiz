339980f2a9ff60daae3cd06a4967580b7e5b9b8f
Marek <mdaw@poczta.fm>
2023-01-28 17:47:59 +0100

    code optimalization

--- main.py
+++ main.py
@@ -2,18 +2,17 @@ import random
 import sys
 from Questions import Quiz
 
-# TODO result comparison - result would be saved after game in xlx file, but i dont know what to compare,
-#  correct answer or ratio.
+
 print('Welcome to quiz. There are a total of 15 questions. You can always end quiz by typing: quit. GL HF')
 print('How many question you would like to answer?')
 a = len(Quiz)
-q_num = int(input('You can chose up %s questions:' % a).strip())  # create exception ?
+q_num = int(input('You can chose up %s questions:' % a).strip())
 
 q = random.sample(list(Quiz.items()), k=q_num)
 player_correct_ans = 0
 
 for num, (question, options) in enumerate(q, start=1):
-    print('\nQuestion %s:' % num)  # is there possibility to make some delay between questions?
+    print('\nQuestion %s:' % num)
     print(question)
     correct_ans = options[0]
     sorted_options = random.sample(options, k=len(options))
@@ -23,14 +22,10 @@ for num, (question, options) in enumerate(q, start=1):
     if player_answer == "quit":
         print('Correct answers: %s, out of %s question' % (player_correct_ans, num - 1))
         sys.exit()
-    while not player_answer.isdigit():  # temp exception handling, improved version in test.py waiting for improve
+    while player_answer not in ['1','2','3','4']:
         print('That\'s not valid answer. Input number from 1 to 4.')
         player_answer = input('Your answer:').strip()
     player_answer = int(player_answer) - 1
-    while player_answer not in range(4):
-        print('That\'s not valid answer. Input number from 1 to 4.')
-        player_answer = input('Your answer:').strip()
-        player_answer = int(player_answer) - 1
     answer = sorted_options[player_answer]
     if answer == correct_ans:
         player_correct_ans += 1
